518cbfe9059e083c1ad223da8bc3a26ff205861c
OHHHHHHei <2859611604@qq.com>
2025-04-18 01:02:09 +0800

    添加向右循环移位注释

--- midterm-exam/2.circulate.c
+++ midterm-exam/2.circulate.c
@@ -10,14 +10,14 @@ int main(void)
     scanf("%i", &shift);
 
     int temp = decimal;
-    while (temp > 0)
+    while (temp > 0)//十进制转换二进制
     {
         binary[length] = temp % 2;
         temp = temp / 2;
         length++;
     }
 
-    if(length == 0)
+    if(length == 0)//考虑等于0的情况
     {
         length = 1;
     }
@@ -25,10 +25,10 @@ int main(void)
 
 
 
-    shift = shift % length;
+    shift = shift % length;//确保移位长度小于位数
     if (shift > 0)
     {
-        int temp_binary[32] = {0};
+        int temp_binary[32] = {0};//建立临时数组防止污染
 
         for (int i = 0; i < length; i++)
         {
@@ -37,12 +37,12 @@ int main(void)
 
         for(int i = 0; i < length; i++)
         {
-            int shifted = (i + shift) % length;
+            int shifted = (i + shift) % length;//模运算来确保范围
             binary[i] = temp_binary[shifted];
         }
     }
 
-    for(int i = length - 1;i >= 0; i--)
+    for(int i = length - 1;i >= 0; i--)//倒序输出才是正确的二进制
     {
         printf("%i", binary[i]);
     }
