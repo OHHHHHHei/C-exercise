0e153324426e4933a401617d3a84203aab715411
OHHHHHHei <2859611604@qq.com>
2025-05-27 17:51:13 +0800

    提交作业9.1

--- xmu-homework/9_1.c
+++ xmu-homework/9_1.c
@@ -5,7 +5,28 @@
  
 
 //函数实现写在这里
-
+int isPalindrome(const char *str, char *pch)
+{
+    int length = strlen(str);
+    int same = 0;
+    for (int i = 0; i < length / 2; i++)
+    {
+        if(str[i] != str[length - 1 - i])//逐项比较
+        {
+            same = 1;
+            break;//检测到不是回文立马退出
+        }
+    }
+    if (same == 0)
+    {
+        *pch = str[(length + 1) / 2 - 1];
+        return 1;
+    } else
+    {
+        *pch = str[(length + 1) / 2 - 1];
+        return 0;
+    }
+}
  
 
 int main()
