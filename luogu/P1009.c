7bd01cee94ddf04ae7d8bb49549c59d2098e3994
OHHHHHH <2859611604@qq.com>
2025-10-29 00:22:36 +0800

    make some optimization

--- luogu/P1009.c
+++ luogu/P1009.c
@@ -1,30 +1,55 @@
 #include <stdio.h>
+#include <string.h>
 
-long long factorial(int a)
-{
-    long long result = a;
-    if(result == 1)
-    {
-        return result;
-    }
-    if(result == 0)
-    {
-        return 1;
-    }
-    result = result * factorial(a - 1);
-    return result;
-}
-
+#define MAXN 100000
 
 int main(void)
 {
     int n;
-    scanf("%i", &n);
-    long long totalsum = 0;
+    scanf("%d", &n);
+    
+    int sum[MAXN] = {0};
+    int s_len = 1;
+    
+    int fact[MAXN] = {0};
+    fact[0] = 1;
+    int f_len = 1;
+    
     for (int i = 1; i <= n; i++)
     {
-        totalsum += factorial(i);
+        // 计算 i!（高精度乘法）
+        int carry = 0;
+        for (int j = 0; j < f_len; j++)
+        {
+            int prod = fact[j] * i + carry;
+            fact[j] = prod % 10;
+            carry = prod / 10;
+        }
+        while (carry)
+        {
+            fact[f_len++] = carry % 10;
+            carry /= 10;
+        }
+        
+        // 将 i! 加到 sum（高精度加法）
+        carry = 0;
+        for (int j = 0; j < f_len || carry; j++)
+        {
+            int s = carry;
+            if (j < s_len) s += sum[j];
+            if (j < f_len) s += fact[j];
+            sum[j] = s % 10;
+            carry = s / 10;
+            if (j >= s_len) s_len++;
+        }
     }
-    printf("%lld", totalsum);
+    
+    // 倒序输出
+    for (int i = s_len - 1; i >= 0; i--)
+    {
+        printf("%d", sum[i]);
+    }
+    printf("\n");
+    
     return 0;
 }
